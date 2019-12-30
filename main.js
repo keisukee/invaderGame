fd8b8d3d1e2e22e7b108808e0f77a67276b7d853
keisuke ohashi <kog71903@gmail.com>
2019-12-30 16:14:57 +0900

    [update]スペースキー打鍵で弾が発射できるように

--- main.js
+++ main.js
@@ -1,24 +1,55 @@
 let invaders = document.getElementsByClassName('invader');
 
-let invader1 = invaders[0];
+// let invader1 = invaders[0];
 
 let posY = 0;
 let posX = 0;
 let distance = 0;
 
-timeout();
+let invader1 = document.getElementById('invader1');
+let invader2 = document.getElementById('invader2');
 
-function timeout() {
+invaderMove();
+
+function invaderMove() {
   setTimeout(function () {
     distance += 10;
-    move(distance);
-    timeout();
-  }, 50);
+    move(invader1, distance, 0);
+    move(invader2, distance, 30);
+    // move('invader2', distance, 50);
+    // move('invader1', distance, 0);
+    // move('invader2', distance, 50);
+    invaderMove();
+  }, 300);
 }
 
-function move(newPosY) {
+function bulletMove(element) {
+  setTimeout(function () {
+    distance += 10;
+    // move('invader1', distance, 0);
+    // move('invader2', distance, 50);
+    bulletMove();
+  }, 300);
+}
+
+function move(element, newPosY, newPosX) {
   posY = newPosY;
-  document.getElementById("invader").style.left = posX;
-  document.getElementById("invader").style.top  = posY;
+  posX = newPosX;
+  element.style.left = posX;
+  element.style.top  = posY;
 }
 
+function shoot(id) {
+  let bullet = '<i class="fas fa-grip-lines-vertical">' + '</i>';
+  document.getElementById(id).insertAdjacentHTML('beforeend', bullet);
+}
+
+document.onkeydown = function (e){
+	if(!e) e = window.event; // レガシー
+  let key_code = e.keyCode;
+  
+  // スペースキーで発射
+  if (key_code === 32) {
+    shoot('starship');
+  }
+};
