5d61ad843f378df1d8cc45323afa4a9b60a08b8c
keisuke ohashi <kog71903@gmail.com>
2019-12-30 17:45:16 +0900

    [update]弾丸とインベーダーの座標計算と簡易の当たり判定を追加

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 let invaders = document.getElementsByClassName('invader');
 
-// let invader1 = invaders[0];
-
 let posY = 0;
 let posX = 0;
 let distance = 0;
@@ -14,31 +12,36 @@ invaderMove();
 function invaderMove() {
   setTimeout(function () {
     distance += 10;
-    move(invader1, distance, 0);
-    move(invader2, distance, 30);
-    // move('invader2', distance, 50);
-    // move('invader1', distance, 0);
-    // move('invader2', distance, 50);
+    move(invader1, 0, distance);
+    move(invader2, 30, distance);
     invaderMove();
   }, 300);
 }
 
-function bulletMove(element, d) {
+function bulletMove(element, x, y) {
   setTimeout(function () {
-    d += 30;
-    move(element, d, 100);
-    // move('invader2', distance, 50);
-    bulletMove(element, d);
-  }, 1000);
+    y -= 1;
+    move(element, x, y);
+    let bulletLocation = detectMomentLocation(element);
+    let invaderLocation = detectMomentLocation(invader1);
+    let distanceOfBulletAndInvaderX = Math.abs(bulletLocation[0] - invaderLocation[0]);
+    let distanceOfBulletAndInvaderY = Math.abs(bulletLocation[1] - invaderLocation[1]);
+    if (distanceOfBulletAndInvaderX < 20 && distanceOfBulletAndInvaderY < 20) {
+      alert('bang!');
+    }
+    bulletMove(element, x, y);
+  }, 1);
 }
 
-function move(element, newPosY, newPosX) {
+// 要素を動かす
+function move(element, newPosX, newPosY) {
   posY = newPosY;
   posX = newPosX;
   element.style.left = posX;
   element.style.top  = posY;
 }
 
+// 弾丸を発射
 function shoot(id) {
   let bullet = '<i class="fas fa-grip-lines-vertical bullets">' + '</i>';
   document.getElementById(id).insertAdjacentHTML('beforeend', bullet);
@@ -46,16 +49,53 @@ function shoot(id) {
 
 document.onkeydown = function (e){
 	if(!e) e = window.event; // レガシー
-  let key_code = e.keyCode;
-  
-  // スペースキーで発射
-  if (key_code === 32) {
+  let keyCode = e.keyCode;
+
+  // console.log(keyCode);
+
+  // スペースキー: 32, s: 83 スペースキーだと、打鍵するときに画面が動く可能性あり
+  if (keyCode === 83) {
     shoot('starship');
     let bullets = document.getElementsByClassName('bullets');
     let bulletCount = bullets.length;
-    console.log(bulletCount);
     let movingBullet = bullets[bulletCount - 1];
-    // console.log(movingBullet);
-    bulletMove(movingBullet, 10);
+    // detectLocation(movingBullet);
+    bulletMove(movingBullet, 10, 300);
   }
 };
+
+// let targetElement = document.getElementById('invader1');
+// detectLocation(targetElement);
+function detectLocation(targetElement) {
+  setInterval( function() {
+    let clientRect = targetElement.getBoundingClientRect();
+  
+    // 画面内の位置
+    let x = clientRect.left;
+    let y = clientRect.top;
+  
+    // ページ内の位置
+    let px = window.pageXOffset + clientRect.left;
+    let py = window.pageYOffset + clientRect.top;
+    let location = [px, py];
+
+    console.log('location', location);
+    return location;
+  }, 10);
+}
+
+function detectMomentLocation(targetElement) {
+  let clientRect = targetElement.getBoundingClientRect();
+
+  // 画面内の位置
+  let x = clientRect.left;
+  let y = clientRect.top;
+
+  // ページ内の位置
+  let px = window.pageXOffset + clientRect.left;
+  let py = window.pageYOffset + clientRect.top;
+  let location = [px, py];
+  console.log('targetElement', targetElement);
+  console.log('location', location);
+  return location;
+}
