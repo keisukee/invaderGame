00d0a4724680870ecd14e83858407cf10fe8eda2
keisuke ohashi <kog71903@gmail.com>
2019-12-31 20:43:44 +0900

    [update]生成するインベーダー全てに当たり判定を実装, 宇宙船を動かして弾丸を発射できるように

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function invaderPopup() {
   newInvaderImg.setAttribute("class", "invader");
   newInvader.style.position = 'absolute';
 
-  newInvader.style.top = getRandomInt(100) * 10;
+  newInvader.style.top = 0;
   newInvader.style.left = getRandomInt(100) * 10;
 
   newInvader.appendChild(newInvaderImg);
@@ -54,20 +54,41 @@ function invaderMove(element) {
   }, 1000);
 }
 
-function bulletMove(element, x, y) {
+function bulletMove(bullet, x, y) {
   setTimeout(function () {
-    y -= 1;
-    move(element, x, y);
-    let bulletLocation = detectMomentLocation(element);
-    let invaderLocation = detectMomentLocation(invader1);
-    let distanceOfBulletAndInvaderX = Math.abs(bulletLocation[0] - invaderLocation[0]);
-    let distanceOfBulletAndInvaderY = Math.abs(bulletLocation[1] - invaderLocation[1]);
-    if (distanceOfBulletAndInvaderX < 15 && distanceOfBulletAndInvaderY < 15) {
-      element.parentNode.removeChild(element);
-      invader1.parentNode.removeChild(invader1);
-    }
-    bulletMove(element, x, y);
-  }, 1);
+    y -= 3;
+    move(bullet, x, y);
+    // let bulletLocation = detectMomentLocation(bullet);
+    // let invaderLocation = detectMomentLocation(invader1);
+    // let distanceOfBulletAndInvaderX = Math.abs(bulletLocation[0] - invaderLocation[0]);
+    // let distanceOfBulletAndInvaderY = Math.abs(bulletLocation[1] - invaderLocation[1]);
+    // if (distanceOfBulletAndInvaderX < 15 && distanceOfBulletAndInvaderY < 15) {
+    //   bullet.parentNode.removeChild(bullet); // 弾丸が消滅
+    //   invader1.parentNode.removeChild(invader1); // インベーダーが消滅
+    // }
+    bulletMove(bullet, x, y);
+  }, 30);
+}
+
+function detectCollision(bullet, invader) {
+  let bulletLocation = detectMomentLocation(bullet);
+  let invaderLocation = detectMomentLocation(invader);
+  let distanceOfBulletAndInvaderX = Math.abs(bulletLocation[0] - invaderLocation[0]);
+  let distanceOfBulletAndInvaderY = Math.abs(bulletLocation[1] - invaderLocation[1]);
+  // console.log('location', bulletLocation);
+  if (distanceOfBulletAndInvaderX < 15 && distanceOfBulletAndInvaderY < 15) {
+    // console.log('detectCollision');
+    bullet.parentNode.removeChild(bullet); // 弾丸が消滅
+    invader.parentNode.removeChild(invader); // インベーダーが消滅
+  }
+}
+
+function autoCollisionDetection(bullet, invader) {
+  setTimeout(function() {
+    // console.log("hogehoge");
+    detectCollision(bullet, invader);
+    autoCollisionDetection(bullet, invader);
+  }, 50);
 }
 
 // 要素を動かす
@@ -79,9 +100,9 @@ function move(element, newPosX, newPosY) {
 }
 
 // 弾丸を発射
-function shoot(id) {
-  let bullet = '<i class="fas fa-grip-lines-vertical bullets">' + '</i>';
-  document.getElementById(id).insertAdjacentHTML('beforeend', bullet);
+function shoot(insertId, uniqueId) {
+  let bullet = `<i id="bullet-${uniqueId}" class="fas fa-grip-lines-vertical bullets"></i>`;
+  document.getElementById(insertId).insertAdjacentHTML('afterend', bullet);
 }
 
 document.onkeydown = function (e){
@@ -89,30 +110,31 @@ document.onkeydown = function (e){
   let keyCode = e.keyCode;
 
   let starshipCor = detectMomentLocation(starship);
-  console.log('cordinates', starshipCor);
-  console.log('cordinates', starshipCor);
-
   // console.log(keyCode);
 
   // スペースキー: 32, s: 83 スペースキーだと、打鍵するときに画面が動く可能性あり
   if (keyCode === 83) {
-    shoot('starship');
     let bullets = document.getElementsByClassName('bullets');
     let bulletCount = bullets.length;
-    let movingBullet = bullets[bulletCount - 1];
-    // detectLocation(movingBullet);
-    bulletMove(movingBullet, 10, 300);
-  } else if (keyCode === 37) { // ←のとき
 
+    shoot('starship', bulletCount);
+    let launchedBullet = bullets[0];
+
+    bulletMove(launchedBullet, starshipCor[0], starshipCor[1]);
+
+    let invaders = document.getElementsByClassName('invader');
+    for (let i = 0; i < invaders.length; i++) {
+      autoCollisionDetection(launchedBullet, invaders[i]);
+    }
+  } else if (keyCode === 37) { // ←のとき
     move(starship, starshipCor[0] - 10, starshipCor[1]);
   } else if (keyCode === 39) { // →のとき
     move(starship, starshipCor[0] + 10, starshipCor[1]);
-
   }
 };
 
 function detectLocation(targetElement) {
-  setInterval( function() {
+  setInterval(function() {
     let clientRect = targetElement.getBoundingClientRect();
   
     // 画面内の位置
@@ -140,7 +162,5 @@ function detectMomentLocation(targetElement) {
   let px = window.pageXOffset + clientRect.left;
   let py = window.pageYOffset + clientRect.top;
   let location = [px, py];
-  console.log('targetElement', targetElement);
-  console.log('location', location);
   return location;
 }
